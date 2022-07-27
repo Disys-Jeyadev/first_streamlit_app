1c334c5845e85133575a3b28fd9fc53a3622847c
Disys-Jeyadev <95424939+Disys-Jeyadev@users.noreply.github.com>
2022-07-27 20:43:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit as st
 st.title('My Parents New Healthy Diner')
+st.header('Breakfast MENU')
+st.text('Omega & Blueberry OatMeal')
+st.text('Kale Spinach and rocket smoothie')
+st.text('Hard-boild free range eggs')
